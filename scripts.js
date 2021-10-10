4939372a4effbf2a0f2a1df73621c532491bbe9b
sihbackes <simonebackesv@gmail.com>
2021-10-10 12:22:42 +0100

    exercícios de fixação

--- scripts.js
+++ scripts.js
@@ -1,7 +1,77 @@
-//Arrays
-const animals = ["Lion", "Monkey", "Cat"];
+// 1. Declare uma variaável de nome Weight
+//let weight
 
-//acessar valores dentro do Array
-console.log(animals[2]);
+// 2. Que tipo de dado é a variável acima?
+// resposta: string
 
-// Lion é 0, monkey é 1 e cat é 2
+/*
+3.Declare uma variável e atribua valores para cada um dos dados:
+*name: String
+*age: number (integer)
+*stars: numer (float)
+*inSubscribed: Boolean
+*/
+/*resposta:
+let name = "Simone"
+let age = 30
+let stars = 4.8
+let isSubscribed = true
+*/
+
+/*
+4. A variável student abaixo pe de que tipo de dados?
+let student = {};
+console.log(typeof student);
+resposta: object
+4.1 Atribua a ela as mesmas propriedades e valores do exercício 3.
+
+
+let student = {
+  name: "Simone",
+  age: 30,
+  stars: 4.8,
+  isSubscribed: true,
+};
+
+4.2 Mostre no console a seguinte mensagem: <name> de idade <age> pesa <weight> kg.
+Atenção, substitua <name> <age> <weight> pelos valores de cada propriedade do obejto.
+let student = {
+  name: "Simone",
+  age: 30,
+  weight: 5.5,
+  isSubscribed: true,
+};
+console.log(
+  `A ${student.name} de idade ${student.age} pesa ${student.weight} kg.`,
+);
+*/
+//5. Decalre uma variável do tipo Array, de nome students e atribua a ela nenhum valor, ou seja, somente o Array vazio.
+
+//let students = [];
+
+/*6. Reatribua valor para a variável acima, colocando dentro dela ao o objeto student da questão 4. (Não copiar e colar o objeto, mas usar o obejto criado e inseri-lo no Array)
+let student = {
+  name: "Simone",
+  age: 30,
+  weight: 5.5,
+  isSubscribed: true,
+};
+let students = [];*/
+//students = [student];
+
+/*7.Coloque no conole o valor da porsição zero do Array acima.*/
+//console.log(students[0]);
+
+/*8. Crie um novo student e coloque na posição 1 do Array students.
+const john = {
+  name: "John",
+  age: 23,
+  weight: 74.8,
+  isSubscribed: true,
+};
+
+students = [student, john];*/
+
+/*9. Sem rodar o código responda qual é a resposta do código abaixo e por que. Após sua resposta, rode o código e veja se você acertou.*/
+console.log(a);
+var a = 1;
