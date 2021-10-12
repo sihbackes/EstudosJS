67bfa6e237bb65de67c7f187436a8c2a80d68bf1
sihbackes <simonebackesv@gmail.com>
2021-10-12 19:07:23 +0100

    finishing functions

--- functions.js
+++ functions.js
@@ -2,15 +2,54 @@
 //function anonymous
 
 //parâmetros (parameters)
-const sum = function (number1, number2) {
+/*const sum = function (number1, number2) {
   total = number1 + number2;
   return total;
 };
 
 let number1 = 34;
-let number2 = 25;
+let number2 = 25; */
 //sum(number1, number2); // arguments - argumentos
 
-console.log(`o número 1 é ${number1}`);
+/*console.log(`o número 1 é ${number1}`);
 console.log(`o número 2 é ${number2}`);
-console.log(`a soma é ${sum(number1, number2)}`);
+console.log(`a soma é ${sum(number1, number2)}`);*/
+
+// Função é um liquidificador
+
+/*function fazerSuco(fruta1, fruta2) {
+  return fruta1 + fruta2;
+}
+const copo = fazerSuco("banana", "maçã");
+console.log(copo);*/
+
+//function hoisting
+/*sayMyName();
+
+function sayMyName() {
+  console.log("Simone");
+}*/
+
+// arrow function
+/*const sayMyName = () => {
+  console.log("Simone");
+};
+
+sayMyName();*/
+
+//Callback function
+/*function sayMyName(name) {
+  console.log(name);
+}
+
+sayMyName("Simone");*/
+//Function () constructor
+//express]ao new
+//criar um novo objeto
+//this keyword
+
+function Person(name) {
+  this.name = name;
+}
+const simone = new Person("Simone");
+console.log(simone);
