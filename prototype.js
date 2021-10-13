42bc289e59b6595ea19963ed4c45e855c6ed198d
sihbackes <simonebackesv@gmail.com>
2021-10-13 19:24:20 +0100

    anotações

---
+++ prototype.js
@@ -0,0 +1,36 @@
+/*
+Prototype
+* prototype-based language
+* prototype chain
+* __proto__
+*/
+
+/*
+
+Type conversion (typecasting) vc Type coersion
+*Alteração de um tipo de dado para outro tipo
+
+*/
+// Manipulando Strings e Números
+// Transformar String em Número e Número em String
+
+/*
+let string = "123";
+console.log(Number(string));
+let number = 321;
+console.log(String(number));
+*/
+
+// Contar quantos caracteres tem uma palavra e quantos dígitos tem um número
+/*
+let word = "paralelepipedo";
+console.log(word.length);
+
+let number = 1234;
+console.log(String(number).length);
+*/
+
+// Tranformar um número quebrado com 2 casas decimais e trocar ponto por vírgula
+
+let number = 455.45484664;
+console.log(number.toFixed(2).replace(".", ","));
