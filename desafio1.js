e09c359ceaa07497bdb88b3e0da76140297ead4f
sihbackes <simonebackesv@gmail.com>
2021-10-19 19:16:03 +0100

    comentários detalhados dos desafios 1 e 2 Rocketseat

--- desafio1.js
+++ desafio1.js
@@ -9,40 +9,81 @@ Crie um algoritmo que transforme as notas do sistema númerico para sistema de n
 * menor que 60    - F
 */
 
+//Minhas anotações
+
+//Let, const e var são variáveis, no exercício está sendo usada let.
+// && signinifca ou
 function getScore(score) {
-  let scoreA = score >= 90 && score <= 100;
-  let scoreB = score >= 80 && score <= 89;
-  let scoreC = score >= 70 && score <= 79;
-  let scoreD = score >= 60 && score <= 69;
-  let scoreF = score < 60 && score >= 0;
+  /*Vai ser scoreA se a nota for maior ou igual a 90 ou(&&) menor ou igual a 100*/
+  let scoreA = score >= 90 && score <= 100
 
-  let scoreFinal;
+  /*Vai ser scoreB se a nota for maior ou igual a 80 ou(&&) menor ou igual a 89*/
+  let scoreB = score >= 80 && score <= 89
 
+  /*Vai ser scoreC se a nota for maior ou igual a 70 ou(&&) menor ou igual a 79*/
+  let scoreC = score >= 70 && score <= 79
+
+  /*Vai ser scoreD se a nota for maior ou igual a 60 ou(&&) menor ou igual a 69*/
+  let scoreD = score >= 60 && score <= 69
+
+  /*Vai ser scoreF se a nota for menor 60 ou(&&) maior ou igual a 0*/
+  let scoreF = score < 60 && score >= 0
+
+  //Declarada outra variável que foi dada o nome de scoreFinal usando let novamente
+  let scoreFinal
+
+  // aqui chamamos as variáveis declaradas a acima
+  //if significa se e else significa senão(outro)
+  //logo se(scoreA) o scorefinal vai ser = A
   if (scoreA) {
-    scoreFinal = "A";
-  } else if (scoreB) {
-    scoreFinal = "B";
-  } else if (scoreC) {
-    scoreFinal = "C";
-  } else if (scoreD) {
-    scoreFinal = "D";
-  } else if (scoreF) {
-    scoreFinal = "F";
-  } else {
-    scoreFinal = "Nota inválida";
+    scoreFinal = 'A'
+  }
+  //Não sendo scoreA, se for scoreB o scorefinal vai ser = B
+  else if (scoreB) {
+    scoreFinal = 'B'
+  }
+  //Não sendo scoreB, se for scoreC o scorefinal vai ser = C
+  else if (scoreC) {
+    scoreFinal = 'C'
+  }
+  //Não sendo scoreC, se for scoreD o scorefinal vai ser = D
+  else if (scoreD) {
+    scoreFinal = 'D'
+  }
+  //Não sendo scoreD, se for scoreF o scorefinal vai ser = F
+  else if (scoreF) {
+    scoreFinal = 'F'
+  }
+  //Não sendo nenhuma das variáveis vai retornar a mensagem : Nota inválida
+  else {
+    scoreFinal = 'Nota inválida'
   }
 
-  return scoreFinal;
+  //return + o nome da função faz com que o seu resultado retorne para fora da função
+  return scoreFinal
 }
+//Aqui pelo console log você pode chamar a função e obter o retorno.
+//O nome da função está entre parenteses e entre um novo parentes está a nota
+//A nota que está em numeral vai retornar convertida em letras como foi definido na aplicação
+console.log(getScore(101))
+console.log(getScore(-10))
+console.log(getScore(0))
+console.log(getScore(45))
+console.log(getScore(55))
+console.log(getScore(65))
+console.log(getScore(70))
+console.log(getScore(85))
+console.log(getScore(100))
+console.log(getScore(95))
+console.log(getScore(90))
 
-console.log(getScore(101));
-console.log(getScore(-10));
-console.log(getScore(0));
-console.log(getScore(45));
-console.log(getScore(55));
-console.log(getScore(65));
-console.log(getScore(70));
-console.log(getScore(85));
-console.log(getScore(100));
-console.log(getScore(95));
-console.log(getScore(90));
+/*Conditional Statements
+Very often when you write code, you want to perform different actions for different decisions.
+
+You can use conditional statements in your code to do this.
+
+In JavaScript we have the following conditional statements:
+
+Use if to specify a block of code to be executed, if a specified condition is true
+Use else to specify a block of code to be executed, if the same condition is false
+Use else if to specify a new condition to test, if the first condition is false*/
--- desafio2.js
+++ desafio2.js
@@ -7,32 +7,58 @@ Crie um objeto que possuira 2 propriedades, ambas do tipo array:
 Agora, crie uma função que irá calcular o total de receitas e despesas e irá mostrar uma mensagem dizendo se a família está com saldo positivo ou negativo, seguido do valor do saldo.
 */
 
+//let family é o objeto
+//dentro do obejto foram criadas duas propriedades: receitas e despesas
+//array é uma lista, para criar um array você deve usar [] e separar por vírgulas
 let family = {
   incomes: [2500, 3200, 250.43, 360.45],
-  expenses: [320.34, 128.45, 176, 87, 1450],
-};
+  expenses: [320.34, 128.45, 176, 87, 1450]
+}
+
+//criar uma função para soma do array
+//no final da função, vai retornar algum resultado
+//então declaramos uma variável com let de total = 0
+//Usamos zero pq o resultado pode ser A PARTIR DE 0, e não pode ficar sem valor
+//Aqui usamos for of
+//for/of - loops through the values of an iterable object
+//no for of vamos listar os valores do array
+//o total(que é igual a zero) vai ser igual o valor + total
+// for of é um loop, ou seja na primeira rodada o valor do primeiro elemento do array vai ser somado a zero
+//na segunda rodada o total já não vai ser zero, e vai ser somado novamente com o segundo elemento do array
+//assim sucessivamente até somar todos os elementos do array
+//poderia tb ser escrito total += valor
+//o return vai trazer o resultado da somar de todos os elementos do array
 
 function sum(array) {
-  let total = 0;
-  for (let value of array) {
-    total += value;
+  let total = 0
+  for (let valor of array) {
+    total = valor + total
   }
-  return total;
+  return total
 }
+
+//essa function apenas roda a função acima
+// na const calculateIncomes ele vai somar todos os elementos do array incomes dentro da variável family
+//na const calculateExpenses ele vai somar todos os elementos do array expenses dentro da variável family
+//na const total ele vai subtrair os dois resultados
+//const isOk estabelece total igual ou maior que zero
+//a let balanceText mostra "negativo", porém se itsok for positivo, ou seja, maior ou igual a zero o balanceText será "positivo"
+// o console log vai mostrar o resultado concatenado com a mensagem "Seu saldo é R$"
+//toFixed(2) deixa o numeral dos centavos com apenas 2 casas decimais
 
 function calculateBalance() {
-  const calculateIncomes = sum(family.incomes);
-  const calculateExpenses = sum(family.expenses);
+  const calculateIncomes = sum(family.incomes)
+  const calculateExpenses = sum(family.expenses)
 
-  const total = calculateIncomes - calculateExpenses;
+  const total = calculateIncomes - calculateExpenses
 
-  const itsOk = total >= 0;
+  const itsOk = total >= 0
 
-  let balanceText = "negativo";
+  let balanceText = 'negativo'
   if (itsOk) {
-    balanceText = "positivo";
+    balanceText = 'positivo'
   }
-  console.log(`Seu saldo é ${balanceText}:R$ ${total.toFixed(2)}`);
+  console.log(`Seu saldo é ${balanceText}:R$ ${total.toFixed(2)}`)
 }
-
-calculateBalance();
+//aqui estamos executando a função acima
+calculateBalance()
